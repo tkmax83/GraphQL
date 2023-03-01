c8f21ae5dfdbad6d31e2e61dda4b00726e869805
tkmax83 <Bret.Woznicki@kohls.com>
2023-03-01 09:35:16 -0600

    Add files via upload

---
+++ src/main/java/com/example/graphql/GraphqlApplication.java
@@ -0,0 +1,14 @@
+package com.example.graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class GraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlApplication.class, args);
+	}
+
+}
